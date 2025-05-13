5a782704c853f6446ab650b26772341899604e6b
MicheleCasani <casanimichele@gmail.com>
2025-05-13 16:07:11 +0200

    [UPD]

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
 
   const [actresses, setActresses] = useState([])
 
+  const listActor = [...actors, ...actresses]
+
 
   // Funzione per effettuare la chiamata HTTP e recuperare i dati degli attori
   const actorsApi = () => {
@@ -32,7 +34,6 @@ function App() {
         console.error(error) // Mostra errori in console in caso di problemi
       });
 
-
   }
 
   // useEffect che chiama actorsApi() al primo caricamento del componente
@@ -54,26 +55,26 @@ function App() {
         </div>
 
         <div className="row g-4">
-          {[...actors, ...actresses].map((actor) => (
-            <div key={actor.id} className="col-12 col-md-6 col-lg-4">
+          {listActor.map((person) => (
+            <div key={person.id} className="col-12 col-md-6 col-lg-4">
               <div className="card h-100 p-3">
                 <div className="row ">
                   {/* Griglia per mostrare l'immagine degli atttori */}
                   <div className="col-5 d-flex align-items-center">
                     <img
-                      src={actor.image}
-                      alt={actor.name}
+                      src={person.image}
+                      alt={person.name}
                       className="img-fluid rounded"
                     />
                   </div>
                   {/* Griglia per mostrare i dati degli atttori */}
                   <div className="col-7">
                     <div className="card-body">
-                      <h5>{actor.name}</h5>
-                      <p className="card-text"><strong>Nationality:</strong> {actor.nationality}</p>
-                      <p className="card-text"><strong>Birth:</strong> {actor.birth_year}</p>
-                      <p className="card-text"><strong>Biography:</strong> {actor.biography}</p>
-                      <p className="card-text"><strong>Awards:</strong> {actor.awards}</p>
+                      <h5>{person.name}</h5>
+                      <p className="card-text"><strong>Nationality:</strong> {person.nationality}</p>
+                      <p className="card-text"><strong>Birth:</strong> {person.birth_year}</p>
+                      <p className="card-text"><strong>Biography:</strong> {person.biography}</p>
+                      <p className="card-text"><strong>Awards:</strong> {person.awards}</p>
                     </div>
                   </div>
 
