54273ee03dcbf649d72fa41173127c723a8d4c8b
MicheleCasani <casanimichele@gmail.com>
2025-05-13 14:33:11 +0200

    [UPD] milestone 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+
+
 
 
 function App() {
 
+  const actorsApi = () => {
+    axios
+      .get('https://lanciweb.github.io/demo/api/actors/')
+      .then((response) => {
+        console.log(response.data)
+      })
+  }
+
+  useEffect(() => {
+    actorsApi();
+  }, []);
 
   return (
     <>
